4a7643bef122ef366085a146f3667a9464a86795
sandalfon01 <sandalfon01@gmail.com>
2023-05-10 21:57:28 -0500

    falta terminar el trabajo (ノω<。)ノ))☆.。

--- src/assets/main.js
+++ src/assets/main.js
@@ -16,11 +16,11 @@ async function fetchData(urlApi){
     try{
         const videos= await fetchData(API);
         let view = `
-        ${videos.items.map(video=>`
+        ${videos.items.map(video => `
         <div class="group relative">
           <div
             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-            <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
           </div>
           <div class="mt-4 flex justify-between">
             <h3 class="text-sm text-gray-700">
@@ -29,10 +29,10 @@ async function fetchData(urlApi){
             </h3>
           </div>
         </div>
-        `).slice(0,4).join('')}
-        
+        `).slice(0, 4).join('')}        
         `;
-    }catch{
-
+        content.innerHTML = view;
+    }catch (error) {
+      console.log(error);
     }
 })();
